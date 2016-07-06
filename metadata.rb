992a6a8bd4764ea495fe56bb9f110d4f1ff01a60
Artemii Kropachev <artem.kropachev@gmail.com>
2016-07-06 08:20:06 +0000

    new version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures lamp'
 long_description 'Installs/Configures lamp'
 issues_url 'aaa'
 source_url 'bbb'
-version '3.0.5'
+version '3.0.6'
 
 depends 'firewall', '~> 2.4.0'
 depends 'chef-sugar', '>= 0.0.0'
