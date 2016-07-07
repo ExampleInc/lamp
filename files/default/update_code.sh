3173db5636744a24f1d2865ac96aa4711e9a4ce1
Dmitrii Mostovshchikov <Dimitry.Mostovshikov@Li9.com>
2016-07-07 14:06:54 -0500

    Updated the update_code.sh script

--- files/default/update_code.sh
+++ files/default/update_code.sh
@@ -8,14 +8,17 @@ case $1 in
   sim)
     cat index.php.sim > index.php
     cat customer.php.sim.db > customer.php
+    cat vis.1.js > vis.js
   ;;
   vis)
     cat index.php.vis > index.php
     cat customer.php.vis.db > customer.php
+    cat vis.1.js > vis.js
   ;;
   vis2)
     cat index.php.vis2 > index.php
     cat customer.php.vis.db > customer.php
+    cat vis.2.js > vis.js
   ;;
   *)
     echo "Use vis or sim as an argument. exit"; exit 1
