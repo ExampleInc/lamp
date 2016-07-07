b444c3ab848f2568c1bfcdacc97833c340bced6a
Dmitrii Mostovshchikov <Dimitry.Mostovshikov@Li9.com>
2016-07-07 15:54:27 -0500

    Updated get_workload.sh script

--- files/default/gen_workload.sh
+++ files/default/gen_workload.sh
@@ -7,8 +7,8 @@ makeload_on() {
   ab -c 100 -n $reqs $1 &
 }
 
-makeload_on http://acceptance01.example.com/
-makeload_on http://union01.example.com/
-makeload_on http://rehearsal01.example.com/
-makeload_on http://delivered01.example.com/
+makeload_on http://acceptance01.example.com/index.php
+makeload_on http://union01.example.com/index.php
+makeload_on http://rehearsal01.example.com/index.php
+makeload_on http://delivered01.example.com/index.php
 
