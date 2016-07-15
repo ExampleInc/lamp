3e30d9886064b3f8490f797d49f444c4de6f54d4
Dmitrii Mostovshchikov <Dimitry.Mostovshikov@Li9.com>
2016-07-15 00:59:00 -0500

    Updated paths in syntax check

--- .delivery/build-cookbook/recipes/syntax.rb
+++ .delivery/build-cookbook/recipes/syntax.rb
@@ -5,19 +5,13 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::syntax'
 
-ruby_block 'default' do
-  block do
-    puts "Current directory: " + run_context.cookbook_collection[cookbook_name].root_dir
-  end
-end
-
 execute 'index.php' do
-  command 'php -l ../../../files/default/index.php'
+  command 'php -l files/default/index.php'
   action :run
 end
 
 execute 'customer.php' do
-  command 'php -l ../../../files/default/customer.php'
+  command 'php -l files/default/customer.php'
   action :run
 end
 
