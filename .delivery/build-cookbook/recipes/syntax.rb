6d0b90faf730122aa14159f0009045a2840ac828
Dmitrii Mostovshchikov <Dimitry.Mostovshikov@Li9.com>
2016-07-15 00:40:23 -0500

    Added a temporary code for printing path to working directory

--- .delivery/build-cookbook/recipes/syntax.rb
+++ .delivery/build-cookbook/recipes/syntax.rb
@@ -5,6 +5,12 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::syntax'
 
+ruby_block 'default' do
+  block do
+    puts "Current directory: " + run_context.cookbook_collection[cookbook_name].root_dir
+  end
+end
+
 execute 'index.php' do
   command 'php -l ../../../files/default/index.php'
   action :run
