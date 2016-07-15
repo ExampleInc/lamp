df267f410db7fcc745e3d4df8bf83af2128b1c56
Dmitrii Mostovshchikov <Dimitry.Mostovshikov@Li9.com>
2016-07-15 01:24:41 -0500

    Updated something

--- .delivery/build-cookbook/recipes/syntax.rb
+++ .delivery/build-cookbook/recipes/syntax.rb
@@ -5,13 +5,15 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 include_recipe 'delivery-truck::syntax'
 
+cb = run_context.cookbook_collection['cookbook_name']
+
 execute 'index.php' do
-  command 'php -l files/default/index.php'
+  command 'php -l ' + cb.manifest['files'] + '/default/index.php'
   action :run
 end
 
 execute 'customer.php' do
-  command 'php -l files/default/customer.php'
+  command 'php -l ' + cb.manifest['files'] + '/default/customer.php'
   action :run
 end
 
